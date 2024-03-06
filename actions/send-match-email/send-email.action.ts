bc900141aa37948c623b047fb9ad9dfb754b9418
Fernando Gonzalez <fernandomenjivar2011@gmail.com>
2024-03-06 06:44:55 -0600

    chore: add prettier config file

--- actions/send-match-email/send-email.action.ts
+++ actions/send-match-email/send-email.action.ts
@@ -1,41 +1,37 @@
-"use server";
+'use server';
 
-import { MatchPerson } from "@/types/core";
-import { randomNumberFromRange } from "@/utils";
+import { MatchPerson } from '@/types/core';
+import { randomNumberFromRange } from '@/utils';
 
 const getRandomNumberDifferentThanCurrent = (
-	min: number,
-	max: number,
-	current: number
+  min: number,
+  max: number,
+  current: number,
 ): number => {
-	let randomNumber = -1;
-	do {
-		randomNumber = randomNumberFromRange(min, max);
-	} while (randomNumber === current);
+  let randomNumber = -1;
+  do {
+    randomNumber = randomNumberFromRange(min, max);
+  } while (randomNumber === current);
 
-	return randomNumber;
+  return randomNumber;
 };
 
 export async function sendMatchEmail(
-	participants: MatchPerson[]
+  participants: MatchPerson[],
 ): Promise<void> {
-	const matchMap = new Map<MatchPerson, MatchPerson>();
-	const avSecret = [...participants];
+  const matchMap = new Map<MatchPerson, MatchPerson>();
+  const avSecret = [...participants];
 
-	for (let i = 0; i < participants.length; ++i) {
-		const randomSecretIdx = getRandomNumberDifferentThanCurrent(
-			0,
-			avSecret.length - 1,
-			i
-		);
+  for (let i = 0; i < participants.length; ++i) {
+    const randomSecretIdx = getRandomNumberDifferentThanCurrent(
+      0,
+      avSecret.length - 1,
+      i,
+    );
 
-		console.log({ randomSecretIdx });
+    console.log({ randomSecretIdx });
 
-		const deleted = avSecret.splice(randomSecretIdx, 1);
-		matchMap.set(participants[i], deleted[0]);
-	}
-
-	for (const [key, value] of matchMap) {
-		console.log(`${key?.email} gives to ${value?.email}`);
-	}
+    const deleted = avSecret.splice(randomSecretIdx, 1);
+    matchMap.set(participants[i], deleted[0]);
+  }
 }
--- app/new-match/page.tsx
+++ app/new-match/page.tsx
@@ -8,89 +8,89 @@ import { generateUid } from "@/utils";
 import { useState } from "react";
 
 const NewMatchPage = () => {
-	const [participants, setParticipants] = useState<
-		(MatchPerson | Partial<MatchPerson>)[]
-	>([]);
+  const [participants, setParticipants] = useState<
+    (MatchPerson | Partial<MatchPerson>)[]
+  >([]);
 
-	const onAdd = (): void => {
-		setParticipants((prev) => [...prev, { uid: generateUid() }]);
-	};
+  const onAdd = (): void => {
+    setParticipants((prev) => [...prev, { uid: generateUid() }]);
+  };
 
-	const onSave = (uuid: string, values: Partial<MatchPerson>): void => {
-		setParticipants((prev) => {
-			const clone = [...prev];
-			const item = clone.find((participant) => participant.uid === uuid);
+  const onSave = (uuid: string, values: Partial<MatchPerson>): void => {
+    setParticipants((prev) => {
+      const clone = [...prev];
+      const item = clone.find((participant) => participant.uid === uuid);
 
-			if (!!item) {
-				item.name = values.name;
-				item.email = values.email;
-			}
+      if (!!item) {
+        item.name = values.name;
+        item.email = values.email;
+      }
 
-			onAdd();
+      onAdd();
 
-			return clone;
-		});
-	};
+      return clone;
+    });
+  };
 
-	const onRemove = (uuid: string): void => {
-		setParticipants((prev) => {
-			return prev.filter((participant) => participant.uid !== uuid);
-		});
-	};
+  const onRemove = (uuid: string): void => {
+    setParticipants((prev) => {
+      return prev.filter((participant) => participant.uid !== uuid);
+    });
+  };
 
-	const onMatch = async () => {
-		const validMathPeople = participants?.filter(
-			(value) => value?.name && value?.email
-		) as unknown as MatchPerson[];
+  const onMatch = async () => {
+    const validMathPeople = participants?.filter(
+      (value) => value?.name && value?.email,
+    ) as unknown as MatchPerson[];
 
-		await sendMatchEmail(validMathPeople);
-	};
+    await sendMatchEmail(validMathPeople);
+  };
 
-	return (
-		<main className="py-4">
-			<h1 className="text-center text-3xl font-semibold py-8">New Match</h1>
-			<section className="w-full md:max-w-md lg:max-w-xl border mx-auto">
-				{!participants.length && (
-					<p className="text-center my-2 mt-4 font-semibold">
-						Add new participants to your secret santa list!
-					</p>
-				)}
-				<ul className="px-2">
-					{participants?.map((participant) =>
-						participant.name && participant.email ? (
-							<MatchPersonItem
-								key={participant.uid}
-								participant={participant as MatchPerson}
-								onRemove={onRemove}
-								onSave={onSave}
-							/>
-						) : (
-							<MatchNewParticipantItem
-								uuid={participant.uid || ""}
-								onSave={onSave}
-								onRemove={onRemove}
-								key={participant.uid}
-							/>
-						)
-					)}
-				</ul>
+  return (
+    <main className="py-4">
+      <h1 className="text-center text-3xl font-semibold py-8">New Match</h1>
+      <section className="w-full md:max-w-md lg:max-w-xl border mx-auto">
+        {!participants.length && (
+          <p className="text-center my-2 mt-4 font-semibold">
+            Add new participants to your secret santa list!
+          </p>
+        )}
+        <ul className="px-2">
+          {participants?.map((participant) =>
+            participant.name && participant.email ? (
+              <MatchPersonItem
+                key={participant.uid}
+                participant={participant as MatchPerson}
+                onRemove={onRemove}
+                onSave={onSave}
+              />
+            ) : (
+              <MatchNewParticipantItem
+                uuid={participant.uid || ""}
+                onSave={onSave}
+                onRemove={onRemove}
+                key={participant.uid}
+              />
+            ),
+          )}
+        </ul>
 
-				<div className="p-4 pt-8">
-					<Button
-						className="bg-primary text-white py-3 px-8 rounded-md w-full"
-						onClick={onAdd}
-					>
-						Add participant
-					</Button>
-					{participants.length > 1 && (
-						<Button variant="success" className="mt-4" onClick={onMatch}>
-							Match!
-						</Button>
-					)}
-				</div>
-			</section>
-		</main>
-	);
+        <div className="p-4 pt-8">
+          <Button
+            className="bg-primary text-white py-3 px-8 rounded-md w-full"
+            onClick={onAdd}
+          >
+            Add participant
+          </Button>
+          {participants.length > 1 && (
+            <Button variant="success" className="mt-4" onClick={onMatch}>
+              Match!
+            </Button>
+          )}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default NewMatchPage;
